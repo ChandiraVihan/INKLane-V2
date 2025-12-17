ea3cda7de41cfe2b1f860593d254a03933689652
Vihan Chandira <chandiravihan@gmail.com>
2025-12-17 13:29:17 +0530

    increase-image-size

--- api/index.js
+++ api/index.js
@@ -28,7 +28,11 @@ const PORT = 3001;
 
 // 2. Essential Middleware
 app.use(cors());
-app.use(express.json());
+// app.use(express.json());
+
+// INCREASE LIMIT TO 50MB
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // 3. Database Connection
 mongoose.connect(process.env.MONGO_URI)
